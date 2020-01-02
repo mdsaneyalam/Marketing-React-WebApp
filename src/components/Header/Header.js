bcc55f1f4b5dd15457d224322cf2605d0d9cd538
Md Saney Alam <codecamper@outlook.com>
2020-01-01 19:31:13 -0500

    intro updated

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -3,20 +3,20 @@ import React from 'react';
 const header = () => {
     return(
         <header id="header">
-            <div class="container-fluid">
+            <div className="container-fluid">
 
-            <div id="logo" class="pull-left">
-                <h1><a href="#intro" class="scrollto">compilia</a></h1>
+            <div id="logo" className="pull-left">
+                <h1><a href="#intro" className="scrollto">compilia</a></h1>
                 
                 <a href="#intro"><img src="img/logo.png" alt="" title="" /></a>
             </div>
 
             <nav id="nav-menu-container">
-                <ul class="nav-menu">
-                <li class="menu-active"><a href="#intro">Home</a></li> 
+                <ul className="nav-menu">
+                <li className="menu-active"><a href="#intro">Home</a></li> 
                 <li><a href="#training">Training</a></li>
                 <li><a href="#team">Team</a></li>
-                <li class="menu-has-children"><a href="">About</a>
+                <li className="menu-has-children"><a href="">About</a>
                     <ul>
                     <li><a href="#">Process</a></li>
                     <li><a href="#about">Heritage</a></li>
--- src/components/Intro/Intro.js
+++ src/components/Intro/Intro.js
@@ -3,7 +3,44 @@ import React from 'react';
 const intro = () => {
     return(
         <section id="intro">
-                
+            <div className="intro-container">
+                <div id="introCarousel" className="carousel  slide carousel-fade" data-ride="carousel">
+
+                    <ol className="carousel-indicators"></ol>
+
+                    <div className="carousel-inner" role="listbox">
+                        
+
+                        
+
+
+                    <div className="carousel-item">
+                        <div className="carousel-background"><img src="./intro-carousel/4.jpg" alt="Inage"/></div>
+                            <div className="carousel-container">
+                                <div className="carousel-content">
+                                    <h2>JavaScript SDET Masters classNameroom Course</h2>
+                                    <p>120+ Hours of classNameroom learning with expert mentors</p>
+                                    <a href="#featured-services" className="btn-get-started scrollto">Get Started</a>
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+
+
+
+                    <a className="carousel-control-prev" href="#introCarousel" role="button" data-slide="prev">
+                        <span className="carousel-control-prev-icon ion-chevron-left" aria-hidden="true"></span>
+                        <span className="sr-only">Previous</span>
+                    </a>
+
+                    <a className="carousel-control-next" href="#introCarousel" role="button" data-slide="next">
+                        <span className="carousel-control-next-icon ion-chevron-right" aria-hidden="true"></span>
+                        <span className="sr-only">Next</span>
+                    </a>
+
+                </div>
+            </div>    
         </section>
         
     );
